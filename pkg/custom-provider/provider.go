5b289e31f64afc99651abfe52e5390cdf02ef334
Andrew Drake <andrew@frontapp.com>
2021-07-06 20:23:01 -0700

    Remove unnecessary log line.

--- pkg/custom-provider/provider.go
+++ pkg/custom-provider/provider.go
@@ -226,7 +226,6 @@ func (p *prometheusProvider) listObjectNames(mapper apimeta.RESTMapper, client d
 	}
 
 	// Use informer to avoid expensve API server pod list call
-	klog.Infof("Listing pods in namespace %s with selector %s", namespace, selector)
 	pods, err := p.podLister.Pods(namespace).List(selector)
 	if err != nil {
 		return nil, err
